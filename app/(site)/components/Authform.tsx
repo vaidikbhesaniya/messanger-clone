5e6804b607b2c8de1514a2dfd1b2ae533cc41266
Vaidik_187 <vaidikbhesaniya1212@gmail.com>
2023-10-01 20:29:48 +0530

    1.0.1

--- app/(site)/components/Authform.tsx
+++ app/(site)/components/Authform.tsx
@@ -3,75 +3,121 @@
 import Input from '@/app/components/inputs/Input';
 import Button from '@/app/components/Button';
 import React, { useCallback, useState } from 'react'
+import { BsGithub, BsGoogle } from "react-icons/bs"
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-type Varient='LOGIN'| 'REGISTER'
+import AuthSocialButton from './AuthSocialButton';
+type Varient = 'LOGIN' | 'REGISTER'
 export default function Authform() {
     const [varient, setvarient] = useState<Varient>('LOGIN');
-const [isloading, setisloading] = useState(false)
+    const [isloading, setisloading] = useState(false)
 
-const toggleVarient=useCallback(()=>{
+    const toggleVarient = useCallback(() => {
 
-if(varient==='LOGIN'){
-    setvarient('REGISTER')
-}
-else{
-    setvarient('LOGIN')
-}
+        if (varient === 'LOGIN') {
+            setvarient('REGISTER')
+        }
+        else {
+            setvarient('LOGIN')
+        }
 
-},[varient])
+    }, [varient])
 
-const {
-    register,handleSubmit,
-    formState:{
-        errors
+    const {
+        register, handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<FieldValues>({
+
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    })
+
+    const onsubmit: SubmitHandler<FieldValues> = (data) => {
+        setisloading(true);
+        if (varient === 'REGISTER') {
+            //axios rregister
+        }
+
+        if (varient === "LOGIN") {
+            //axios login
+        }
     }
-}=useForm<FieldValues>({
 
-    defaultValues:{
-        name:'',
-        email:'',
-        password:''
+    const socialaction = (action: string) => {
+        setisloading(true);
+
+        //next auth social sign in
     }
-})
-
-const onsubmit:SubmitHandler<FieldValues>=(data)=>{
-setisloading(true);
-if(varient==='REGISTER'){
-    //axios rregister
-}
-
-if(varient==="LOGIN"){
-    //axios login
-}
-}
-
-const socialaction=(action:string)=>{
-setisloading(true);
-
-//next auth social sign in
-}
     return (
-    <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
 
-<div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+            <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
 
-<form action="" className='space-y-6' onSubmit={handleSubmit(onsubmit)}>
+                <form action="" className='space-y-6' onSubmit={handleSubmit(onsubmit)}>
 
-    {varient==="REGISTER" && (
-        <Input id='email' lable='Email' register={register} errors={errors}></Input>
+                    {varient === "REGISTER" && (
+                        <Input id='email' lable='Email' register={register} errors={errors}></Input>
 
-    )}
+                    )}
 
-<Input id='email' type='email' lable='Email address' register={register} errors={errors}></Input>
-<Input id='password' type='password' lable='Password' register={register} errors={errors}></Input>
+                    <Input id='email' type='email' lable='Email address' register={register} errors={errors}></Input>
+                    <Input id='password' type='password' lable='Password' register={register} errors={errors}></Input>
 
 
-<div>
-    <Button >Sign in</Button>
-</div>
-</form>
-</div>
+                    <div>
+                        <Button fullWidth type='submit' disabled={isloading}>{varient === "LOGIN" ? "Sign in" : "Register"}</Button>
+                    </div>
+                </form>
 
-    </div>
-  )
+
+
+                <div className='mt-6'>
+
+                    <div className='relative'>
+                        <div className='absolute inset-0 flex items-center'>
+
+                            <div className='w-full border-t border-gray-300' />
+
+
+
+
+                        </div>
+                        <div className='relative flex justify-center text-sm  '>
+                            <span className='bg-white text-gray-500 px-2'>Or continue with</span>
+
+                        </div>
+                    </div>
+
+                    <div className='mt-6 flex gap-2 '>
+
+                        <AuthSocialButton onClick={() => socialaction('github')} icon={BsGithub}></AuthSocialButton>
+
+                        <AuthSocialButton onClick={() => socialaction('google')} icon={BsGoogle}></AuthSocialButton>
+
+                    </div>
+
+                </div>
+
+                <div className='flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500'>
+                    <div >
+                        {varient === "LOGIN" ? "New to Messanger?" : "Already have an account?"}
+                    </div>
+
+                    <div
+                        onClick={toggleVarient}
+                        className='underline cursor-pointer'
+                    >
+                        {varient === "LOGIN" ? 'Create an account' : "Log in"}
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,19 +4,19 @@ import Authform from './components/Authform'
 
 export default function Home() {
   return (
-    <div className='text-sky-600 flex w-[100vw] h-[100vh] flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100'>
+    <div className='text-sky-600 flex w-[100vw] h-[100%] flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100'>
 
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
 
         <Image
-        alt='logo'
-        src="/images/logo.png"
-        height={45}
-        width={45}
-        className='mx-auto w-auto '
+          alt='logo'
+          src="/images/logo.png"
+          height={45}
+          width={45}
+          className='mx-auto w-auto '
         ></Image>
-        <h2 className='mt-6 text-center text-xl font-bold tracking-tight text-gray-900 mb-7'>
-Sign in to your account
+        <h2 className='mt-6 text-center text-xl font-bold tracking-tight text-gray-900 mb-2'>
+          Sign in to your account
         </h2>
       </div>
       <Authform></Authform>
